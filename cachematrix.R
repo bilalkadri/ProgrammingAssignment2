4674e3cbae5a3087cba3a038f46b4605ef3eef21
M BILAL KADRI <bilal.kadri@gmail.com>
2014-09-20 12:28:01 +0500

    Revert "a brief message about the update/commit"
    This reverts commit b230288a6e5a22a85bac669f2abcbdb3b4dad33e.

--- cachematrix.R
+++ cachematrix.R
@@ -5,17 +5,6 @@
 
 makeCacheMatrix <- function(x = matrix()) {
 
-  m <- NULL
-  set <- function(y) {
-    x <<- y
-    m <<- NULL
-  }
-  get <- function() x
-  setinverse <- function(solve) m <<- solve
-  getinverse <- function() m
-  list(set = set, get = get,
-       setinverse = setinverse,
-       getinverse = getinverse)
 }
 
 
@@ -23,17 +12,4 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
-  m <- x$getinverse()
-  if(!is.null(m)) {
-    message("getting cached data")
-    return(m)
-  }
-  data <- x$get()
-  
-  m <- solve(data, ...)
-  
-  
-  x$setinverse(m)
-  m
-  
 }
